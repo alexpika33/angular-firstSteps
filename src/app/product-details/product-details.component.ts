9f7cfc2e86d60d623961c2e2eaa0e50fabace34b
alexpika33 <alexnupe@gmail.com>
2021-04-07 12:26:07 +0200

    Finalizado ejemplo (carrito, shipping y formulario de nombre & direccion)

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { products } from "../products";
+import { CartService } from "../cart.service";
 
 @Component({
   selector: "app-product-details",
@@ -9,12 +10,21 @@ import { products } from "../products";
 })
 export class ProductDetailsComponent implements OnInit {
   product;
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
+  addToCart(product) {
+    // definimos metodo addTocart usando el metodo creado previamente en el servicio
+    this.cartService.addToCart(product);
+    window.alert("Your product has been added to the cart");
+  }
 
   ngOnInit() {
+    //primero cogemos el producto de la ruta actual
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get("productId"));
-
+    // buscamos el producto del id cogido de la ruta anterior
     this.product = products.find(product => product.id === productIdFromRoute);
   }
 }
